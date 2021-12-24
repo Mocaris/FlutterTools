65431a560f3e3b47050afdfdfaa0207670b02b6b
mocaris <imocaris@outlook.com>
2021-12-24 16:46:50 +0800

    1.0.11 release

--- src/main/java/com/mocaris/plugin/flutter/sync/Utils.kt
+++ src/main/java/com/mocaris/plugin/flutter/sync/Utils.kt
@@ -5,16 +5,18 @@ import com.intellij.notification.*
 object Utils {
 
     fun notificationSticky(title: String, message: String, type: NotificationType) {
-        val groupManager = NotificationGroupManager.getInstance()
+        /*val groupManager = NotificationGroupManager.getInstance()
         val notificationGroup = groupManager.getNotificationGroup("notifi_sticky_balloon")
-        val notification = notificationGroup.createNotification(title, message, type)
+        val notification = notificationGroup.createNotification(title, message, type)*/
+        val notification = Notification("notifi_sticky_balloon", title, message, type)
         Notifications.Bus.notify(notification)
     }
 
     fun notificationBalloon(title: String, message: String, type: NotificationType) {
-        val groupManager = NotificationGroupManager.getInstance()
-        val notificationGroup = groupManager.getNotificationGroup("notifi_balloon")
-        val notification = notificationGroup.createNotification(title, message, type)
+        /* val groupManager = NotificationGroupManager.getInstance()
+         val notificationGroup = groupManager.getNotificationGroup("notifi_balloon")
+         val notification = notificationGroup.createNotification(title, message, type)*/
+        val notification = Notification("notifi_balloon", title, message, type)
         Notifications.Bus.notify(notification)
     }
 
