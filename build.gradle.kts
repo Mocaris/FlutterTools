29b70e10d5708517c99476e781595133eadfb63c
Mocaris <imocaris@outlook.com>
2025-09-15 17:05:23 +0800

    update README.MD

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,8 @@ dependencies {
         create("IC", "2023.2")
         testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
 
-      // Add necessary plugin dependencies for compilation here, example:
-      // bundledPlugin("com.intellij.java")
+        // Add necessary plugin dependencies for compilation here, example:
+        // bundledPlugin("com.intellij.java")
     }
 }
 
@@ -34,7 +34,23 @@ intellijPlatform {
         }
 
         changeNotes = """
-            Initial version
+        ## 1.0.12 
+        - update plugin and use yaml config
+
+        ## 1.0.11 
+        - bug fixed,Using the Notification Api 
+
+        ## 1.0.10 
+        - update plugin 
+
+        ## 1.0.9 
+        - bug fixed 
+
+        ## 1.0.8 
+        - sort sync files 
+
+        ## 1.0.7 
+        - fixed window path bug 
         """.trimIndent()
     }
 }
